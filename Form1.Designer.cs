ebd8881414db1919093433bc3805a5469aefb66f
atsugula <111021816+atsugula@users.noreply.github.com>
2023-11-25 10:55:51 -0500

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace game_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            tiempoJuego = new System.Windows.Forms.Timer(components);
+            txtPuntaje = new Label();
+            pbJugador = new PictureBox();
+            tituloInicio = new Label();
+            btnEmpezar = new Label();
+            ((System.ComponentModel.ISupportInitialize)pbJugador).BeginInit();
+            SuspendLayout();
+            // 
+            // tiempoJuego
+            // 
+            tiempoJuego.Interval = 20;
+            tiempoJuego.Tick += eventoPrincipalTiempoJuego;
+            // 
+            // txtPuntaje
+            // 
+            txtPuntaje.AutoSize = true;
+            txtPuntaje.Font = new Font("Showcard Gothic", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            txtPuntaje.Location = new Point(12, 535);
+            txtPuntaje.Name = "txtPuntaje";
+            txtPuntaje.Size = new Size(79, 21);
+            txtPuntaje.TabIndex = 1;
+            txtPuntaje.Text = "Puntaje: 0";
+            txtPuntaje.UseCompatibleTextRendering = true;
+            txtPuntaje.Visible = false;
+            // 
+            // pbJugador
+            // 
+            pbJugador.Image = Properties.Resources.nave_espacial;
+            pbJugador.Location = new Point(333, 499);
+            pbJugador.Name = "pbJugador";
+            pbJugador.Size = new Size(55, 50);
+            pbJugador.SizeMode = PictureBoxSizeMode.StretchImage;
+            pbJugador.TabIndex = 2;
+            pbJugador.TabStop = false;
+            pbJugador.Visible = false;
+            // 
+            // tituloInicio
+            // 
+            tituloInicio.AutoSize = true;
+            tituloInicio.Font = new Font("Comic Sans MS", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            tituloInicio.Location = new Point(145, 191);
+            tituloInicio.Name = "tituloInicio";
+            tituloInicio.Size = new Size(490, 90);
+            tituloInicio.TabIndex = 4;
+            tituloInicio.Text = "Space Invader";
+            // 
+            // btnEmpezar
+            // 
+            btnEmpezar.AutoSize = true;
+            btnEmpezar.BackColor = SystemColors.ActiveCaption;
+            btnEmpezar.Font = new Font("Comic Sans MS", 27.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnEmpezar.Location = new Point(274, 302);
+            btnEmpezar.Name = "btnEmpezar";
+            btnEmpezar.Size = new Size(174, 51);
+            btnEmpezar.TabIndex = 5;
+            btnEmpezar.Text = "Empezar";
+            btnEmpezar.Click += presionaEmpezar;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDarkDark;
+            ClientSize = new Size(734, 561);
+            Controls.Add(btnEmpezar);
+            Controls.Add(tituloInicio);
+            Controls.Add(pbJugador);
+            Controls.Add(txtPuntaje);
+            Name = "Form1";
+            Text = "Space Invader";
+            KeyDown += teclaPresionada;
+            KeyUp += teclaArriba;
+            ((System.ComponentModel.ISupportInitialize)pbJugador).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tiempoJuego;
+        private Label txtPuntaje;
+        private PictureBox pbJugador;
+        private Label tituloInicio;
+        private Label btnEmpezar;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,309 @@
+using System.Numerics;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace game_v2
+{
+    public partial class Form1 : Form
+    {
+
+        // Movimiento del jugador
+        bool movIzquierda, movDerecha;
+        // Velocidad del jugador
+        int jugadorVelocidad = 12;
+        // Velocidad de los enemigos
+        int enemigosVelocidad = 5;
+        // Puntaje del jugador
+        int puntajeContador = 0;
+        // cuando generar las balas enemigas
+        int tiempoBalasEnemigas = 300;
+        // cantidad enemigos
+        int cantidadEnemigos = 10;
+
+        // cuadro de imagenes de los invasores, es un array, para que sean varias
+        PictureBox[] invasoresArray;
+
+        bool disparo;
+        bool juegoPerdido;
+        bool empezoJuego;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Nueva clase Jugador
+        public class Jugador
+        {
+            // Propiedades del jugador
+            public string Nombre { get; set; }
+            public int Puntos { get; set; }
+            public int Velocidad { get; set; }
+            public int Vidas { get; set; }
+
+            // Constructor
+            public Jugador(string nombre, int puntos, int velocidad, int vidas)
+            {
+                Nombre = nombre;
+                Puntos = puntos;
+                Velocidad = velocidad;
+                Vidas = vidas;
+            }
+
+        }
+
+        private void teclaPresionada(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                movIzquierda = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                movDerecha = true;
+            }
+        }
+
+        private void teclaArriba(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                movIzquierda = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                movDerecha = false;
+            }
+            if (e.KeyCode == Keys.Space && disparo == false)
+            {
+                disparo = true;
+                crearDisparo("jugadorDisparo");
+            }
+            if (e.KeyCode == Keys.Enter && juegoPerdido == true)
+            {
+                quitarTodo();
+                crearJuego();
+            }
+        }
+
+        private void presionaEmpezar(object sender, EventArgs e)
+        {
+            empezoJuego = true;
+            crearJuego();
+        }
+
+        private void crearInvasores()
+        {
+            invasoresArray = new PictureBox[cantidadEnemigos];
+
+            int left = 0;
+
+            for (int i = 0; i < invasoresArray.Length; i++)
+            {
+
+                invasoresArray[i] = new PictureBox();
+                invasoresArray[i].Size = new Size(60, 50);
+                invasoresArray[i].Image = Properties.Resources.alien; // cambiar imagen del enemigo
+                invasoresArray[i].Top = 0;
+                invasoresArray[i].Tag = "invasor";
+                invasoresArray[i].Left = left;
+                invasoresArray[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                this.Controls.Add(invasoresArray[i]);
+                left = left - 80;
+
+            }
+
+        }
+
+        // Funcion para definir o comenzar el juego
+        private void crearJuego()
+        {
+            // Pongo los elementos visibles
+            if (empezoJuego)
+            {
+                // Ponemos los personajes visibles
+                pbJugador.Visible = true;
+                txtPuntaje.Visible = true;
+                // Borramos el inicio del juego
+                btnEmpezar.Visible = false;
+                tituloInicio.Visible = false;
+
+                txtPuntaje.Text = "Puntaje: 0";
+                puntajeContador = 0;
+                juegoPerdido = false;
+
+                tiempoBalasEnemigas = 300;
+                enemigosVelocidad = 5;
+                disparo = false;
+
+                crearInvasores();
+                tiempoJuego.Start();
+            }
+
+        }
+
+        private void perdioJuego(string message)
+        {
+            juegoPerdido = true;
+            tiempoJuego.Stop();
+            txtPuntaje.Text = $"Score: {puntajeContador} {message}";
+        }
+
+        private void quitarTodo()
+        {
+
+            foreach (PictureBox i in invasoresArray)
+            {
+                this.Controls.Remove(i);
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((string)x.Tag == "jugadorDisparo" || (string)x.Tag == "enemigoDisparo")
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+
+        }
+
+        private void crearDisparo(string tipoDisparo)
+        {
+            PictureBox bala = new PictureBox();
+            bala.Image = Properties.Resources.bala_1;
+            bala.Size = new Size(24, 24);
+            bala.Tag = tipoDisparo;
+            // Estara en la mitad de la imagen del jugador
+            // Como si recreara un rayo o algo así
+            bala.Left = pbJugador.Left + pbJugador.Width / 2;
+
+            if ((string)bala.Tag == "jugadorDisparo")
+            {
+                bala.Top = pbJugador.Top - 20;
+            }
+            else if ((string)bala.Tag == "enemigoDisparo")
+            {
+                bala.Top = -100;
+            }
+
+            this.Controls.Add(bala);
+
+            // Poner la bala, por encima de todo
+            bala.BringToFront();
+
+        }
+
+        private void eventoPrincipalTiempoJuego(object sender, EventArgs e)
+        {
+
+            txtPuntaje.Text = $"Puntaje: {puntajeContador}";
+
+            if (movIzquierda && empezoJuego)
+            {
+                pbJugador.Left -= jugadorVelocidad;
+            }
+
+            if (movDerecha && empezoJuego)
+            {
+                pbJugador.Left += jugadorVelocidad;
+            }
+
+            tiempoBalasEnemigas -= 10;
+
+            if (tiempoBalasEnemigas < 1)
+            {
+                tiempoBalasEnemigas = 300;
+                crearDisparo("enemigoDisparo");
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "invasor")
+                {
+
+                    x.Left += enemigosVelocidad;
+
+                    if (x.Left > 730)
+                    {
+                        x.Top += 65;
+                        x.Left = -80;
+                    }
+
+                    if (x.Bounds.IntersectsWith(pbJugador.Bounds))
+                    {
+                        perdioElJuego("Has sido invadido por los invasores, ¡ahora estás triste!");
+                    }
+
+                    foreach (Control y in this.Controls)
+                    {
+
+                        if (y is PictureBox && (string)y.Tag == "jugadorDisparo")
+                        {
+                            if (y.Bounds.IntersectsWith(x.Bounds))
+                            {
+                                this.Controls.Remove(x);
+                                this.Controls.Remove(y);
+                                puntajeContador += 1;
+                                disparo = false;
+                            }
+                        }
+
+                    }
+
+                }
+
+                if (x is PictureBox && (string)x.Tag == "jugadorDisparo")
+                {
+                    x.Top -= 20;
+
+                    if (x.Top < 15)
+                    {
+                        this.Controls.Remove(x);
+                        disparo = false;
+                    }
+
+                }
+
+                // Si falla el tiro por aca cambiamos
+                if (x is PictureBox && (string)x.Tag == "enemigoDisparo")
+                {
+                    x.Top += 20;
+
+                    if (x.Top > 620)
+                    {
+                        this.Controls.Remove(x);
+                    }
+
+                    if (x.Bounds.IntersectsWith(pbJugador.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        perdioElJuego("Te han matado. Ahora estarás triste para siempre.");
+                    }
+
+                }
+
+            }
+
+            // Validamos si vamos a ganar
+            if (puntajeContador > 8)
+            {
+                enemigosVelocidad = 12;
+            }
+
+            if (puntajeContador == invasoresArray.Length)
+            {
+                perdioElJuego("Woohoo Felicidades, ¡Mantenlo a salvo!");
+            }
+        }
+
+        private void perdioElJuego(string message)
+        {
+            juegoPerdido = true;
+            tiempoJuego.Stop();
+            txtPuntaje.Text = $"Score: {puntajeContador} {message}";
+        }
+
+    }
+}
